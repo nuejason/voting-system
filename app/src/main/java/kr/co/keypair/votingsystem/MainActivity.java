a6fb96d45ee1ca5f10712ea78b0d9d05eb6f201a
regalfaith <regalfaith@gmail.com>
2018-07-10 10:54:08 +0900

    First commit

---
+++ app/src/main/java/kr/co/keypair/votingsystem/MainActivity.java
@@ -0,0 +1,42 @@
+package kr.co.keypair.votingsystem;
+
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import org.web3j.crypto.Credentials;
+import org.web3j.protocol.Web3j;
+import org.web3j.protocol.Web3jFactory;
+import org.web3j.protocol.http.HttpService;
+
+public class MainActivity extends AppCompatActivity {
+
+    private final String msContractAddr = "0x0101010101010101010101010101010101010101";
+    private final String msPrikey = "0x0000000000000000000000000000000000000000000000000000000000000000";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Credentials credentials = Credentials.create(msPrikey);
+        Web3j web3 = Web3jFactory.build(new HttpService("https://rinkeby.infura.io/your-api-key"));
+
+        Handler handler = new Handler();
+        handler.post(new Runnable() {
+            @Override
+            public void run() {
+                new Thread() {
+                    @Override
+                    public void run() {
+                        try {
+
+                        } catch (Exception e) {
+                            e.toString();
+                        }
+                    }
+                }.start();
+            }
+        });
+
+    }
+}
