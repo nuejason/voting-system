ce14299059cae32b7b8bc0a5f3c078795104ea9f
kim <hkkim0103@naver.com>
2018-07-10 17:29:43 +0900

    MainActivity add key

--- app/src/main/java/kr/co/keypair/votingsystem/MainActivity.java
+++ app/src/main/java/kr/co/keypair/votingsystem/MainActivity.java
@@ -11,7 +11,7 @@ import org.web3j.protocol.http.HttpService;
 public class MainActivity extends AppCompatActivity {
 
     private final String msContractAddr = "0x0101010101010101010101010101010101010101";
-    private final String msPrikey = "0x0000000000000000000000000000000000000000000000000000000000000000";
+    private final String msPrikey = "0x627c3cced38c0068f8ac17b989fc166551dd061400998585e80fd4ef6251be07";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,7 +19,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Credentials credentials = Credentials.create(msPrikey);
-        Web3j web3 = Web3jFactory.build(new HttpService("https://rinkeby.infura.io/your-api-key"));
+        Web3j web3 = Web3jFactory.build(new HttpService("https://rinkeby.infura.io/swGGKC97MU0pqiKuFUpA"));
 
         Handler handler = new Handler();
         handler.post(new Runnable() {
