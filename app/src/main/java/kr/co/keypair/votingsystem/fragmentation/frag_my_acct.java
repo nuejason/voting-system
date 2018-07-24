bdb668845262a3cb1712ca4167fd9b43807c15fc
kim <hkkim0103@naver.com>
2018-07-24 17:31:36 +0900

    frag_my_acct

--- app/src/main/java/kr/co/keypair/votingsystem/fragmentation/frag_my_acct.java
+++ app/src/main/java/kr/co/keypair/votingsystem/fragmentation/frag_my_acct.java
@@ -1,13 +1,13 @@
 package kr.co.keypair.votingsystem.fragmentation;
 
-import android.content.Context;
-import android.net.Uri;
-import android.os.Bundle;
-import android.support.v4.app.Fragment;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import kr.co.keypair.votingsystem.R;
+        import android.content.Context;
+        import android.net.Uri;
+        import android.os.Bundle;
+        import android.support.v4.app.Fragment;
+        import android.view.LayoutInflater;
+        import android.view.View;
+        import android.view.ViewGroup;
+        import kr.co.keypair.votingsystem.R;
 
 public class frag_my_acct extends Fragment {
 
