958a2a028c1b4e9807574d9f83ca2645f9be8517
hyunjikoh <hyunjikoh@naver.com>
2018-07-31 11:41:31 +0900

    merge

--- app/src/main/java/kr/co/keypair/votingsystem/fragmentation/frag_betting.java
+++ app/src/main/java/kr/co/keypair/votingsystem/fragmentation/frag_betting.java
@@ -3,11 +3,6 @@ package kr.co.keypair.votingsystem.fragmentation;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentPagerAdapter;
-import android.support.v4.app.FragmentTransaction;
-import android.support.v4.view.ViewPager;
-import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -17,24 +12,13 @@ import android.widget.ImageView;
 import android.widget.RadioButton;
 import android.widget.TextView;
 import android.widget.Toast;
-
-import com.astuetz.PagerSlidingTabStrip;
-
-import org.web3j.crypto.Credentials;
-import org.web3j.protocol.Web3j;
-import org.web3j.protocol.Web3jFactory;
 import org.web3j.protocol.core.RemoteCall;
 import org.web3j.protocol.core.methods.response.TransactionReceipt;
-import org.web3j.protocol.http.HttpService;
-
 import java.math.BigInteger;
 import java.util.concurrent.ExecutionException;
-
-import kr.co.keypair.votingsystem.Betting;
 import kr.co.keypair.votingsystem.MainActivity;
 import kr.co.keypair.votingsystem.R;
-import kr.co.keypair.votingsystem.fragmentation.my_betting.betting1;
-import kr.co.keypair.votingsystem.fragmentation.my_betting.betting2;
+
 
 public class frag_betting extends Fragment {
 
@@ -55,14 +39,6 @@ public class frag_betting extends Fragment {
                              Bundle savedInstanceState) {
         getActivity().setTitle("배팅하기");
         v = inflater.inflate(R.layout.fragment_frag_betting, container, false);
-<<<<<<< HEAD
-=======
-        msPrikey = "627c3cced38c0068f8ac17b989fc166551dd061400998585e80fd4ef6251be07";
-        final String msContractAddr = "0xd53cd226a573de91fa2897b004bb6b2a9d3117be";
-        final Credentials credentials = Credentials.create(msPrikey);
-        final Web3j web3 = Web3jFactory.build(new HttpService("https://rinkeby.infura.io/swGGKC97MU0pqiKuFUpA"));
-        final Betting contract = Betting.load(msContractAddr, web3, credentials, gasPrice, gasLimit);
->>>>>>> 7288f6a63fa3c25b476998191c20b30b31004ca4
 
         Bundle extra = getArguments();
         int image1 = extra.getInt("image1");
